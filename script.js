b046c767cca8e6584ef2208a271e580e1026f4e3
Kenny <50640060+Feierstein1@users.noreply.github.com>
2020-01-01 15:50:42 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	
+	$("#home").click(function(){
+		document.location.href = "index.html";
+	});
+	$("#resume").click(function(){
+		document.location.href = "resume.html";
+	});
+	$("#projects").click(function(){
+		document.location.href = "projects.html";
+	});
+	$("#hobbies").click(function(){
+		document.location.href = "hobbies.html";
+	});
+});

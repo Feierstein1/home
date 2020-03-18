610f86c6c33f76b24a0bb56b1e78abbee950d9c5
Kenny <50640060+Feierstein1@users.noreply.github.com>
2020-03-17 21:30:05 -0400

    Update theme.js

--- Projects/NavBar/theme.js
+++ Projects/NavBar/theme.js
@@ -3,19 +3,24 @@ const themeMap = {
   light: 'solar',
   solar: 'dark'
 };
-
+if(localStorage.getItem('theme') == 'undefined'){
+	const theme = localStorage.setItem('theme', themeMap['dark']);
+	console.log("set theme");
+}
 const theme = localStorage.getItem('theme');
 const bodyClass = document.body.classList;
 theme && bodyClass.add(theme);
+console.log(theme && bodyClass.add(theme));
 
 function toggleTheme() {
   console.log("toggle");
   const current = localStorage.getItem('theme');
   console.log("current", current);
   const next = themeMap[current];
-
+  console.log("next", next);
   bodyClass.replace(current, next);
   localStorage.setItem('theme', next);
+  console.log(current);
 }
 
 document.getElementById('themeButton').onclick = toggleTheme;

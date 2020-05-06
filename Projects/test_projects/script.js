8db78756030e46e447e1df1b14a24d0d66dcc2a1
Kenny <50640060+Feierstein1@users.noreply.github.com>
2020-05-05 21:07:56 -0400

    LMS update

---
+++ Projects/test_projects/script.js
@@ -0,0 +1,26 @@
+const themeMap = {
+  dark: "light",
+  light: "normal",
+  normal: "dark",
+};
+if (localStorage.getItem("theme") == "undefined") {
+  const theme = localStorage.setItem("theme", themeMap["normal"]);
+  console.log("set theme");
+}
+const theme = localStorage.getItem("theme");
+const bodyClass = document.body.classList;
+theme && bodyClass.add(theme);
+console.log(theme && bodyClass.add(theme));
+
+function toggleTheme() {
+  console.log("toggle");
+  const current = localStorage.getItem("theme");
+  console.log("current", current);
+  const next = themeMap[current];
+  console.log("next", next);
+  bodyClass.replace(current, next);
+  localStorage.setItem("theme", next);
+  console.log(current);
+}
+
+document.getElementById("settings").onclick = toggleTheme;
